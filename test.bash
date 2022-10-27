a720293ebb55741782195409fd84285e60da1ea7
Yasuda Marin <s21C1130TL@s.chibakoudai.jp>
2022-10-27 12:35:54 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Marin Yasuda
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus_stdin
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus_stdin)
+
+[ "${out}" = 15 ]
+
+[ "$res" = 0 ] && echo OK
+exit $res
