358bd3cb56e0e77f02c35fce1c6baaaf3975345a
Yasuda Marin <s21C1130TL@s.chibakoudai.jp>
2022-11-30 20:06:50 +0900

    Support integer only calculation

--- test.bash
+++ test.bash
@@ -1,3 +1,27 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus_stdin
+ng () {
+	      echo NG at Line $1
+	            res=1
+	    }
+	    
+	    res=0
+	    
+	    ### I/O TEST ###
+	    out=$(seq 5 | ./plus_stdin)
+	    [ "${out}" = 15 ] || ng ${LINENO}
+	   
+	    ### STRANGE INPUT ###
+	    out=$(echo あ | ./plus_stdin)
+	    [ "$?" = 1 ]      || ng ${LINENO}
+	    [ "${out}" = "" ] || ng ${LINENO}
+
+	    out=$(echo | ./plus) #空文字
+	    [ "$?" = 1 ]      || ng ${LINENO}
+	    [ "${out}" = "" ] || ng ${LINENO}
+		      　 
+
+	    [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+	    exit $res
